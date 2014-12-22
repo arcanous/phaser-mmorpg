30822fc222a863b4a77cf16e413a05d303cf732a
Harijs Deksnis <harijs.deksnis@gmail.com>
2014-12-22 16:37:34 +0100

    Codebase update
    Latest phaser library
    Fullscreen to fit whole browser window
    Desert map tileset

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var TopDownGame = TopDownGame || {};
 
-TopDownGame.game = new Phaser.Game(500, 500, Phaser.AUTO, '');
+TopDownGame.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
 
 TopDownGame.game.state.add('Boot', TopDownGame.Boot);
 TopDownGame.game.state.add('Preload', TopDownGame.Preload);
