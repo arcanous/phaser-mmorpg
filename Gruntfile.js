5bf1ebeff2574d761eaf639aeffffd62ae3097d2
Harijs Deksnis <harijs.deksnis@gmail.com>
2015-01-02 15:45:26 +0100

    Grunt added

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    require('time-grunt')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        meta: {
+            version: '0.2.0'
+        },
+        banner: '/*! PhaserMMORPG - v<%= meta.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '* http://mmorpg.astrocity.lv/\n' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+            'YOUR_NAME; Licensed MIT */\n',
+        // Task configuration.
+        concat: {
+            main: {
+                src: ['js/**/*.js', '!js/lib/**/*.js'],
+                dest: 'build/PhaserMMORPG.concat.js'
+            }
+        },
+        jshint: {
+            main : ['js/**/*.js', '!js/lib/**/*.js'],
+            options: {
+                browser: true,
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                forin: true,
+                newcap: true,
+                noempty: true,
+                plusplus: true,
+                strict: true,
+                trailing: true,
+                white: true, //harsh but needed. Douglas Crockford style
+                reporter:'jslint',
+                reporterOutput: 'jshint.xml'
+            }
+            
+
+        },  
+
+
+        uglify: {
+            options: {
+                mangle: true
+            },
+            main: {
+                src: 'build/PhaserMMORPG.concat.js',
+                dest: 'build/PhaserMMORPG.min.js'
+            }
+        },       
+
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Tasks.
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+
+};
